a8408deabf0a8a5a9d74c0b4d339cf1fbd65d3dc
Deependra Shivhare <125219210+Deependra108@users.noreply.github.com>
2024-10-04 14:32:31 +0000

    IIFE

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,11 @@
+// Immediately Invoked Function Expressions (IIFE)
+
+(function chai(){
+      // named IIFE
+    console.log(`DB Connected`);
+})();
+
+
+(() => {
+    console.log(`DB CONNECTED TWO`);
+})()
